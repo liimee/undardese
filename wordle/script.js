d8ac14c614ac41ae2ce02c07114cd435a3099414
ta <alt3753.7@gmail.com>
2022-01-26 19:16:29 +0700

    base thing ig?

---
+++ wordle/script.js
@@ -0,0 +1,8 @@
+const a = b = 6;
+for(let c = 0; c<a; c++) {
+  for(let e = 0; e<b; e++) {
+    const f = document.createElement('span');
+    f.className = 'g';
+    document.querySelector('#gr').appendChild(f)
+  }
+}
