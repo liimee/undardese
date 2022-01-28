b946295d767e6482c9b100a5b6e181bfe816ef95
Ectogen‮ <sndeepak@icloud.com>
2022-01-28 11:52:19 +0530

    Remove words to move

--- wordle/script.js
+++ wordle/script.js
@@ -38,45 +38,7 @@ keys.up = ["qwērtyūīōp", "āsdfghjkł", "zxcvb̃m", "↥⌫"].map((e) => e.s
 keys.low[0].unshift("õ");
 
 let h = 0;
-const wordlist = [
-  //I've marked the meaning btw
-  'vārthe',//word
-  'vērnge',//roots
-  'erakkœ',//lower (v.)
-  'kuripa',//especially, specifically
-  'kaddhõ',//letter (the mail kind of letter)
-  "mārāme",//without changing
-  "pūmāle",//garland
-  "pałeye",//old
-  "pudhus",//new
-  "chinne",//small
-  "mīnawr",//fisherman
-  "mīnnge",//fish (pl.)
-  "thanni",//water
-  "thīane",//extinguish
-  "kādhal",//love
-  "kodume",//tyranny, torture, much annoyance
-  "chakre",//sugar
-  "uppāne",//salty
-  "mandōd",//skull
-  "rkānge",//are there, exist
-  "kālnge",//legs
-  "kainge",//arms, hands
-  "norērl",//lung
-  "edheyõ",//heart
-  "manshe",//human
-  "velang",//animal
-  "kāinge",//vegetables
-  "surkne",//shrunken, compressed
-  "persāk",//expand
-  "pirike",//to seperate, to open (a plastic package)
-  "vedike",//to explode
-  "kāpāth",//help, save, rescue
-  "valake",//to grow (something/someone), to brush (teeth)
-  "valare",//(for someone/something) to grow
-  "parave",//bird
-  "maraōd"//tree bark
-].map(e => e.replace(/õ/g, "õ"))
+
 var word = wordlist[Math.floor(Math.random() * wordlist.length)];
 function makeKeys(layout = "low") {
   kb.innerText = "";
