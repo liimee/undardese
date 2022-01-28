edb92449e5efddf65d7b35c61c921f4fc39c69e8
Ectogen‮ <sndeepak@icloud.com>
2022-01-28 11:52:49 +0530

    Create words.js

---
+++ wordle/words.js
@@ -0,0 +1,39 @@
+const wordlist = [
+  //I've marked the meaning btw
+  'vārthe',//word
+  'vērnge',//roots
+  'erakkœ',//lower (v.)
+  'kuripa',//especially, specifically
+  'kaddhõ',//letter (the mail kind of letter)
+  "mārāme",//without changing
+  "pūmāle",//garland
+  "pałeye",//old
+  "pudhus",//new
+  "chinne",//small
+  "mīnawr",//fisherman
+  "mīnnge",//fish (pl.)
+  "thanni",//water
+  "thīane",//extinguish
+  "kādhal",//love
+  "kodume",//tyranny, torture, much annoyance
+  "chakre",//sugar
+  "uppāne",//salty
+  "mandōd",//skull
+  "rkānge",//are there, exist
+  "kālnge",//legs
+  "kainge",//arms, hands
+  "norērl",//lung
+  "edheyõ",//heart
+  "manshe",//human
+  "velang",//animal
+  "kāinge",//vegetables
+  "surkne",//shrunken, compressed
+  "persāk",//expand
+  "pirike",//to seperate, to open (a plastic package)
+  "vedike",//to explode
+  "kāpāth",//help, save, rescue
+  "valake",//to grow (something/someone), to brush (teeth)
+  "valare",//(for someone/something) to grow
+  "parave",//bird
+  "maraōd"//tree bark
+].map(e => e.replace(/õ/g, "õ"))
