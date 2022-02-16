bc8b3afcd97a3fc402f311b805e16c79d3d2f0bf
Electogenius <69585636+Electogenius@users.noreply.github.com>
2022-02-16 13:51:32 +0530

    well guess I'm done

--- course.js
+++ course.js
@@ -1,5 +1,5 @@
 const data = {
-  basics1: [
+    basics1: [
     [
       'far fa-hand-point-down noun',
       'dhœ',
@@ -26,7 +26,7 @@ const data = {
       'okay'
     ]
   ],
-  basics2: [
+    basics2: [
     [
       'fas fa-binoculars',
       'pār',
@@ -53,7 +53,7 @@ const data = {
       'drink'
     ]
   ],
-  basics3: [
+    basics3: [
     [
       'fas fa-user singular',
       'nī',
@@ -85,12 +85,12 @@ const data = {
       'small'
     ]
   ],
-  sentences1:[
-    [//todo split obnoxiously large tip into smaller sections
-      'word','fas fa-plus', '-õ', 'and, too, also', 'Use it after a word, like nī (you) -> nīõ (you too). This suffix makes nasal sounds (ã) become non-nasals followed by an ‘n’ (nã + õ = nānõ).'
+    sentences1: [
+    [ //todo: split obnoxiously large tip into smaller sections?
+      'word', 'fas fa-plus', '-õ', 'and, too, also', 'Use it after a word, like nī (you) -> nīõ (you too). This suffix makes nasal sounds (ã) become non-nasals followed by an ‘n’ (nã + õ = nānõ).'
     ],
     [
-    	'sent','this and that', ["dhõ adhõ", "dhõ adhœ", "dhõ adh"], ["dh","adh","œ","õ"], "The 'õ' suffix removes any œ sound at the end of a word (dhœ + õ = dhõ)."
+    	'sent', 'this and that', ['dhõ adhõ', 'dhõ adhœ', 'dhõ adh'], ['dh', 'adh', 'œ', 'õ'], 'The \'õ\' suffix removes any œ sound at the end of a word (dhœ + õ = dhõ). This applies to all suffixes starting with vowels'
     ],
     [
         'y/n', 'fas fa-plus', ['-ã', 'and, too, also'], false
@@ -99,20 +99,55 @@ const data = {
         'word', 'fas fa-dog', 'nāi/nāy', 'Dog'
     ],
     [
-        'a/b', ["adhõ","nāyõ"], 2, 'fas fa-sun', 'The dog too'
+        'a/b', ['adhõ', 'nāyõ'], 2, 'fas fa-sun', 'The dog too'
     ],
     [
-        'sent','nīõ andhe nāiõ', ["you and that dog"], ["dog","me","see","that","you","and"]
+        'sent', 'nīõ andhe nāiõ', ['you and that dog'], ['dog', 'me', 'see', 'that', 'you', 'and']
     ],
     [
-        'y/n', 'fas fa-binocular', ['pār','see'],true
+        'y/n', 'fas fa-binoculars', ['pār', 'see'], true
     ]
   ],
-  data: {
-    _sections: 2,
-    basics1: ['Basics 1', 'far fa-sun', 1],
-    basics2: ['Basics 2', 'fas fa-walking', 1],
-    basics3: ['Basics 3', 'fas fa-users', 1],
-    sentences1: ['Sentences 1', 'fas fa-icons',2,true]
-  }
+    sentences2: [
+        [
+            'word', 'fas fa-arrow-down', '-e', 'Object marker', 'Add it to the end of the whatever or whoever the verb of the sentence is performed on/to'
+        ],
+        [
+            'a/b', ['kadi', 'sāpd'], 2, 'fas fa-utensils', 'Eat'
+        ],
+        [
+            'sent', 'Look at this', ['dhe pār', 'dhe pārœ', 'pār dhe', 'pārœ dhe'], ['kudi','sāpd','dh','e','pār','œ','õ'], 'Undardese’s word order is Subject-Object-Verb, so the word with the -e suffix comes before the verb. Also the order isn’t all that strict.'
+        ],
+        [
+            'sent', 'Eat it', ['adhe sāpd', 'dhe sāpd', 'sāpd adhe', 'sāpd dhe', 'adhe sāpdœ', 'dhe sāpdœ', 'sāpdœ adhe', 'sāpdœ dhe'], ['sāpād', 'œ', 'kūpd', 'sāpd', 'e', 'dh', 'adh'], 'Undardese doesn’t have a word for ‘it’ so you have to use ‘this’ or ‘that’'
+        ],
+        [
+            'word', 'fas fa-clock', '-r', 'Simple present tense marker', 'Added after verbs. There has to be a suffix marking who/what is doing it after this'
+        ],
+        [
+            'word', 'fas fa-user-circle', 'æ̃', 'First person singular marker', 'Added after a tense marker. So \'-ræ̃\' added after a verb means you are doing it, and it is happenning now.'
+        ],
+        [
+            'a/b', ['sāpdræ̃', 'sāpdr', 'sāpdæ̃'], 1, 'fas fa-utensils', 'I eat'
+        ],
+        [
+            'sent', 'I see it',
+            [
+                'nã adhe pārræ̃',
+                'nã dhe pārræ̃'
+            ],
+            [
+                'nī', 'dh', 'ed', 'pār', 'õ', 'æ̃',
+                'nã', 'adh', 'r', 'e'
+            ], 'Don\'t forget, Subject-Object-Verb. The subject here is you. Subject don\' have any suffixed'
+        ]
+    ],
+    data: {
+        _sections: 2,
+        basics1: ['Basics 1', 'far fa-sun', 1],
+        basics2: ['Basics 2', 'fas fa-walking', 1],
+        basics3: ['Basics 3', 'fas fa-users', 1],
+        sentences1: ['Sentences 1', 'fas fa-icons', 2, true],
+        sentences2: ['Sentences 2', 'fas fa-binoculars', 2, true]
+    }
 }
