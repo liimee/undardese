76c4f1f4ae985c40a73f5d7e4b264496d7e99097
liimee <alt3753.7@gmail.com>
2022-02-25 07:40:05 +0700

    block previous word

--- wordle/getword.js
+++ wordle/getword.js
@@ -1,4 +1,8 @@
 import wordlist from './words.js'
 
+const w = wordlist;
+const c = new TextDecoder("utf-8");
+const d = Deno.readFileSync("./wordle/word");
+w.splice(w.indexOf(c.decode(d).trim()), 1)
 Deno.writeTextFileSync('./wordle/wordlist.json', JSON.stringify(wordlist));
-Deno.writeTextFileSync('./wordle/word', wordlist[Math.floor(Math.random() * wordlist.length)])
+Deno.writeTextFileSync('./wordle/word', w[Math.floor(Math.random() * wordlist.length)])
