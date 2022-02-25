fad4f27b8a9d30126120c1d02308dca25f4da816
liimee <alt3753.7@gmail.com>
2022-02-25 07:44:24 +0700

    test

--- wordle/getword.js
+++ wordle/getword.js
@@ -1,8 +1,8 @@
 import wordlist from './words.js'
 
+Deno.writeTextFileSync('./wordle/wordlist.json', JSON.stringify(wordlist));
 const w = wordlist;
 const c = new TextDecoder("utf-8");
 const d = Deno.readFileSync("./wordle/word");
 w.splice(w.indexOf(c.decode(d).trim()), 1)
-Deno.writeTextFileSync('./wordle/wordlist.json', JSON.stringify(wordlist));
 Deno.writeTextFileSync('./wordle/word', w[Math.floor(Math.random() * wordlist.length)])
